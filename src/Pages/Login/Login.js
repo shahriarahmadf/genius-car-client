740a92558b6051dd120c8f662298a208f4bc2154
shahriarahmadf <shahriarahmadf@gmail.com>
2023-03-11 23:12:25 +0900

    firebase setup with login page structured

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import img from '../../assets/images/login/login.svg'
+
+const Login = () => {
+    const handleLogin = event => {
+        event.preventDefault();
+    }
+    return (
+        <div className="hero w-full my-20">
+          <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+            <div className="text-center lg:text-left">
+              <img className='w-4/5' src={img} alt="" />
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <h1 className="text-5xl font-bold text-center">Login</h1>
+              
+              <form onSubmit={handleLogin} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input type="text" placeholder="email" className="input input-bordered" />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input type="text" placeholder="password" className="input input-bordered" />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                  </label>
+                </div>
+                <div className="form-control mt-6">
+                    <input type="submit" value="Login" className="btn btn-primary"/>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+    );
+};
+
+export default Login;
