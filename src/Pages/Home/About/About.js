5f5fb182686e7d3292e879725a2de3076dbb00e9
shahriarahmadf <shahriarahmadf@gmail.com>
2023-03-11 22:52:49 +0900

    structured services section

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,7 +4,7 @@ import parts from '../../../assets/images/about_us/parts.jpg'
 
 const About = () => {
     return (
-        <div className="hero min-h-screen bg-base-200">
+        <div className="hero my-20">
             <div className="hero-content flex-col lg:flex-row">
                 
                 <div className='relative w-1/2'>
@@ -14,14 +14,15 @@ const About = () => {
 
                 <div className='w-1/2'>
                     <p className="text-2xl text-orange-600 font-bold">About Us</p>
-                    <h1 className="text-5xl font-bold">
+                    <h1 className="my-5 text-5xl font-bold">
                         We are qualified <br />
                         & of experience <br />
                         in this field
                     </h1>
 
                     <p className="py-6">
-                        There are many variations of passages of lorem ipsum available. But the majority hace suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+                        There are many variations of passages of lorem ipsum available. But the majority hace suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.
+                    </p>
                     <p className="py-6">
                         The Majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
                     </p>
